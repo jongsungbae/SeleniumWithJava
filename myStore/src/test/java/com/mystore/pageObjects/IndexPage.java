8442f4f58afda2898915e96c9560897335132784
jongsungbae <jongsung.jaybae@gmail.com>
2022-11-10 00:53:09 -0500

    indexPage

--- myStore/src/test/java/com/mystore/pageObjects/IndexPage.java
+++ myStore/src/test/java/com/mystore/pageObjects/IndexPage.java
@@ -1,5 +1,32 @@
 package com.mystore.pageObjects;
 
-public class IndexPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class IndexPage {
+	
+	WebDriver ldriver;
+	
+	public IndexPage(WebDriver rdriver) {
+		ldriver = rdriver;
+		PageFactory.initElements(rdriver, this);
+		
+	}
+	
+	@FindBy(name="search")
+	@CacheLookup
+	WebElement searchBox;
+	
+	public void txtSearchBox(String txtSearch) {
+		searchBox.sendKeys(txtSearch);
+	}
+	
+	public boolean searchBoxisVisible() {
+		searchBox.isEnabled();
+		return true;
+	}
+	
 }
---
+++ myStore/src/test/java/com/mystore/testCases/TC_IndexTest_001.java
@@ -0,0 +1,27 @@
+package com.mystore.testCases;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import java.io.IOException;
+import com.mystore.pageObjects.IndexPage;
+
+public class TC_IndexTest_001 extends baseClass{
+	
+	@Test
+	public void indexTest() throws IOException{
+		
+		IndexPage indexPage = new IndexPage(driver);
+		
+		if(driver.getTitle().equals("Your Store")) {
+			Assert.assertTrue(true);
+			logger.info("Index Page is displayed");
+		}else {
+			captureScreen(driver, "IndexPageTest");
+			Assert.assertTrue(false);
+			logger.info("index Page failed");
+		}
+		
+		
+	}
+
+}
